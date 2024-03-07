1e62c12eabe9a0f899f3a63a9de9c8c0a74a4dcb
Balaprasad133 <balaprasadh133@gmail.com>
2024-03-07 11:06:39 +0530

    Updated code of routing part

--- src/app/study/[...lecture]/page.js
+++ src/app/study/[...lecture]/page.js
@@ -1,8 +1,8 @@
 export default function Lecture({params}){
     return(
         <div>
-            <h1> {params.lecture[0]} </h1>
-            <h2>{params.lecture[1            ]}</h2>
+            <h1>Day of the college: {params.lecture[0]} </h1>
+            <h2>Lecture Number :{params.lecture[1]}</h2>
         </div>
     )
 }
